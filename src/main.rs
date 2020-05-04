b0dcfc41360d389ded55aac39723b37e7b33c22c
Bo0km4n <pokopenkatuya@gmail.com>
2020-05-05 04:11:43 +0900

    add Rc & RefCell module

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ mod userrepo;
 
 use user::UserAlias;
 use todo::todo::Todo;
-use userrepo::new_user;
+use userrepo::*;
+use std::cell::RefCell;
+use std::rc::Rc;
+
 fn main() {
     let t: Todo = Todo{
         desc: "AAA".to_string(),
@@ -16,6 +19,14 @@ fn main() {
         name: "Kawabe".to_string(),
     };
 
+    let box_user = Box::new(UserAlias {
+        id: 1,
+        name: "Kawabe".to_string(),
+    });
+    let refc_box_user = Rc::new(RefCell::new(box_user));
+
     let new_u: UserAlias = new_user("Kawabe".to_string());
     println!("user = {:?}, todo = {:?}, new_user = {:?}", u, t, new_u);
+    write_user_to_db(Rc::clone(&refc_box_user));
+    println!("{:?}", Rc::clone(&refc_box_user).borrow_mut());   
 }
--- src/userrepo/mod.rs
+++ src/userrepo/mod.rs
@@ -1,7 +1,13 @@
 use super::user::User;
+use std::rc::Rc;
+use std::cell::RefCell;
 pub fn new_user(name: String) -> User {
     return User {
         name: name,
         id: 1,
     }
+}
+
+pub fn write_user_to_db(req: Rc<RefCell<Box<User>>>) {
+    println!("write user data to db = {:?}", req.borrow_mut());
 }
